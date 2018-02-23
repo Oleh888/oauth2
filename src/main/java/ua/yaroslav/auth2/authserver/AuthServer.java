e4ddedd999589eda84b3e21c70a467dc334cb3eb
Yaruliy <l.yaruk1993@gmail.com>
2018-02-23 15:49:01 +0300

    Deploy

--- src/main/java/ua/yaroslav/auth2/authserver/AuthServer.java
+++ src/main/java/ua/yaroslav/auth2/authserver/AuthServer.java
@@ -1,10 +1,8 @@
 package ua.yaroslav.auth2.authserver;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ua.yaroslav.auth2.datastore.Database;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +11,8 @@ import java.io.IOException;
 @RestController
 public class AuthServer {
     private final Database database;
+    private final String CLIENT_ID = "client";
+    private final String CLIENT_SECRET = "secret";
 
     public AuthServer(Database database) {
         this.database = database;
@@ -27,16 +27,11 @@ public class AuthServer {
         System.out.println(client_id);
         System.out.println(redirect_uri);
         System.out.println(response_type);
-//        String token = Base64.getEncoder().encodeToString("token".getBytes());
-//        database.addToken(token);
-//        System.out.println("Set header: " + "Bearer " + token);
-//        //request.setAttribute("Authorization","Bearer " + token);
-//        response.addHeader("Authorization","Bearer " + token);
-//        //response.setHeader("Authorization","Bearer " + token);
-//
-//        RequestDispatcher rd = request.getRequestDispatcher("/private");
-//        rd.forward(request, response);
-//
-//        //response.sendRedirect("/private");
+
+    }
+
+    @RequestMapping("/")
+    public String getHome(){
+        return "<h3>Hell Yeah Server</h3>";
     }
 }
