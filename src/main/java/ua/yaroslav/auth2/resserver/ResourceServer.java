f4f12a07c9cee3d3f3adb85f0e26fb4241179454
Yaruliy <l.yaruk1993@gmail.com>
2018-02-26 16:26:34 +0300

    Deploy

--- src/main/java/ua/yaroslav/auth2/resserver/ResourceServer.java
+++ src/main/java/ua/yaroslav/auth2/resserver/ResourceServer.java
@@ -1,5 +1,6 @@
 package ua.yaroslav.auth2.resserver;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ua.yaroslav.auth2.datastore.Database;
 import javax.servlet.http.HttpServletRequest;
@@ -15,7 +16,9 @@ public class ResourceServer{
     }
 
     @GetMapping(value = {"/private"})
-    public String getPrivateData(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public String getPrivateData(HttpServletRequest request,
+                                 HttpServletResponse response,
+                                 @RequestParam("token") String token) throws IOException {
         System.out.println(request.getHeader("Authorization"));
         if(request.getHeader("Authorization") == null) {
             System.out.println("Send redirect ot /oauth");
@@ -24,6 +27,8 @@ public class ResourceServer{
         else {
             System.out.println(database.isValidToken(request.getHeader("Authorization").substring(6)));
             if(database.isValidToken(request.getHeader("Authorization").substring(6)))
+                return "private data " + request.getRemoteUser() + "<br>" + request.getHeader("Authorization");
+            else if(database.isValidToken(token))
                 return "private data " + request.getRemoteUser() + "<br>" + request.getHeader("Authorization");
         }
         System.out.println("Header: [" + request.getHeader("Authorization") + "]");
