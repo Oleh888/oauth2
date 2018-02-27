f793671a2d5037d81110a1fc5593570880a3055b
Yaruliy <l.yaruk1993@gmail.com>
2018-02-27 12:21:04 +0300

    Deploy

--- src/main/java/ua/yaroslav/auth2/resserver/ResourceServer.java
+++ src/main/java/ua/yaroslav/auth2/resserver/ResourceServer.java
@@ -6,6 +6,9 @@ import ua.yaroslav.auth2.datastore.Database;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class ResourceServer{
@@ -19,19 +22,28 @@ public class ResourceServer{
     public String getPrivateData(HttpServletRequest request,
                                  HttpServletResponse response,
                                  @RequestParam(value = "token", required = false) String token) throws IOException {
-        System.out.println(request.getHeader("Authorization"));
-        if(request.getHeader("Authorization") == null) {
-            System.out.println("Send redirect ot /oauth");
-            response.sendRedirect("/oauth");
+//        System.out.println(request.getHeader("Authorization"));
+//        if(request.getHeader("Authorization") == null) {
+//            System.out.println("Send redirect ot /oauth");
+//            response.sendRedirect("/oauth");
+//        }
+//        else {
+//            System.out.println(database.isValidToken(request.getHeader("Authorization").substring(6)));
+//            if(database.isValidToken(request.getHeader("Authorization").substring(6)))
+//                return "private data"+ "<br>" + request.getHeader("Authorization");
+//            else if(database.isValidToken(token))
+//                return "private data" + " <br> " + request.getHeader("Authorization");
+//        }
+//        System.out.println("Header: [" + request.getHeader("Authorization") + "]");
+        Map<String, String> map = new HashMap<String, String>();
+
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            System.out.println(key + " -> " + value);
+            map.put(key, value);
         }
-        else {
-            System.out.println(database.isValidToken(request.getHeader("Authorization").substring(6)));
-            if(database.isValidToken(request.getHeader("Authorization").substring(6)))
-                return "private data " + request.getRemoteUser() + "<br>" + request.getHeader("Authorization");
-            else if(database.isValidToken(token))
-                return "private data" + " <br> " + request.getHeader("Authorization");
-        }
-        System.out.println("Header: [" + request.getHeader("Authorization") + "]");
-        return "Token is invalid";
+        return "DATA";
     }
 }
