8936b99b5278a3549866b3095c8a7d5f3c0900ff
Yaruliy <l.yaruk1993@gmail.com>
2018-02-27 13:19:29 +0300

    Deploy

--- src/main/java/ua/yaroslav/auth2/authserver/AuthServer.java
+++ src/main/java/ua/yaroslav/auth2/authserver/AuthServer.java
@@ -55,6 +55,7 @@ public class AuthServer {
                     System.out.println("\nDecoded data:");
                     System.out.println(encoded);
                     database.addToken("SlAV32hkKG");
+                    System.out.println();
                     return "{\n" +
                             "token_type: \"bearer\",\n" +
                             "access_token: \"SlAV32hkKG\",\n" +
