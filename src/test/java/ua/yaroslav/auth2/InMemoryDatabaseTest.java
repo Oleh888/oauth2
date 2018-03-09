0e5b3e3be9cb3341f195c1f03e461b0685ad6a7c
Yaruliy <l.yaruk1993@gmail.com>
2018-03-09 17:15:02 +0300

    In-memory database tests

--- src/test/java/ua/yaroslav/auth2/InMemoryDatabaseTest.java
+++ src/test/java/ua/yaroslav/auth2/InMemoryDatabaseTest.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.LinkedMultiValueMap;
@@ -31,17 +30,11 @@ public class InMemoryDatabaseTest {
     private final String client = "test";
     private final String secret = "test";
 
-    //@Autowired
-    //private ApplicationContext context;
-
     @Autowired
     private PostgreClientStore clientStore;
 
     @Before
     public void setUp() {
-        //PostgreClientStore cs = context.getBean("postgreClientStore", PostgreClientStore.class);
-        //cs.saveClient(new Client(client, secret));
-
         clientStore.saveClient(new Client(client, secret));
         clientStore.saveClient(new Client(client, secret));
     }
