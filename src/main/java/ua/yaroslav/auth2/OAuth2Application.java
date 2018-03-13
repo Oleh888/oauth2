9023ae5dfb874430736cb9b9e8ade56f8e4fd00f
Yaruliy <l.yaruk1993@gmail.com>
2018-03-13 13:52:13 +0300

    Deploy

--- src/main/java/ua/yaroslav/auth2/OAuth2Application.java
+++ src/main/java/ua/yaroslav/auth2/OAuth2Application.java
@@ -2,11 +2,10 @@ package ua.yaroslav.auth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppRunner extends SpringBootServletInitializer {
+public class OAuth2Application {
     public static void main(String[] args) {
-        SpringApplication.run(AppRunner.class, args);
+        SpringApplication.run(OAuth2Application.class, args);
     }
 }
