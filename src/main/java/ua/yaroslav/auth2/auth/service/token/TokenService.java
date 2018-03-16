2c91b7e5b9d637b2be8561409070e2f02d005399
Yaroslav Lukyanenko <l.yaruk1993@gmail.com>
2018-03-16 16:53:47 +0200

    RabbitMQ

---
+++ src/main/java/ua/yaroslav/auth2/auth/service/token/TokenService.java
@@ -0,0 +1,13 @@
+package ua.yaroslav.auth2.auth.service.token;
+
+import ua.yaroslav.auth2.auth.dto.AuthRequestDto;
+import ua.yaroslav.auth2.auth.dto.TokenRequestDto;
+import ua.yaroslav.auth2.auth.dto.TokenResponseDto;
+
+import java.io.IOException;
+
+public interface TokenService {
+    String getCode(AuthRequestDto authRequest);
+    TokenResponseDto getTokensAsJSON(TokenRequestDto tokenRequest) throws IOException;
+    TokenResponseDto getRefreshedTokenAsJSON(TokenRequestDto tokenRequest) throws IOException;
+}
